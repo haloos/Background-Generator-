fc505f3b4fdb65d7d3371b5ec0b349bed26f1654
Jason <haloos@me.com>
2019-03-12 11:56:45 -0400

    addEventListener

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2"); 
 var body = document.getElementById("gradient");
 
-console.log(body);
-
 color1.addEventListener("input", function(){
-    console.log(color1.value);
+    body.style.background = "Linear-gradient(to right, " 
+    + color1.value
+    + ", "
+    + color2.value 
+    + ")"; 
+
 }) 
 
 color2.addEventListener("input", function(){
-  console.log(color2.value);
+  body.style.background = "Linear-gradient(to right, " 
+    + color1.value
+    + ", "
+    + color2.value 
+    + ")"; 
+    
 })
